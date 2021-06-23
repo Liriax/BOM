65c52dcd573a1977d0f60a9d4e8a8275e4a4cae7
Liria <liriaxiaoge@gmail.com>
2021-06-23 18:17:18 +0200

    update requirements.txt

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from ete3 import Tree
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
 import pandas as pd
 import gunicorn
 from dash.dependencies import Input, Output, State
